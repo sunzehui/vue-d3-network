9239905e99eb4aafd930e27cff144541274426d6
阿辉 <oncwnuEXKikeT96xJEFPh9GH37UM@git.weixin.qq.com>
2022-06-08 12:14:04 +0800

    add mouse zoom

--- src/components/svgRenderer.vue
+++ src/components/svgRenderer.vue
@@ -8,191 +8,196 @@
     class="net-svg"
     @mouseup='emit("dragEnd",[$event])'
     @touchend.passive='emit("dragEnd",[$event])'
-    @touchstart.passive=''
     )
+    g.ground(ref="ground")
+      //-> links
+      g.links#l-links
+          path(v-for="link in links"
+            :d="linkPath(link)"
+            :id="link.id"
+            @click='emit("linkClick",[$event,link])'
+            @touchstart.passive='emit("linkClick",[$event,link])'
+            v-bind='linkAttrs(link)'
+            :class='linkClass(link.id)'
+            :style='linkStyle(link)'
+            )
 
-    //-> links
-    g.links#l-links
-        path(v-for="link in links"
-          :d="linkPath(link)"
-          :id="link.id"
-          @click='emit("linkClick",[$event,link])'
-          @touchstart.passive='emit("linkClick",[$event,link])'
-          v-bind='linkAttrs(link)'
-          :class='linkClass(link.id)'
-          :style='linkStyle(link)'
-          )
+      //- -> nodes
+      g.nodes#l-nodes(v-if='!noNodes')
+        template(v-for='(node,key) in nodes')
+          svg(v-if='svgIcon(node)'
+            :key='key'
+            :viewBox='svgIcon(node).attrs.viewBox'
+            :width='getNodeSize(node, "width")'
+            :height='getNodeSize(node, "height")'
+            @click='emit("nodeClick",[$event,node])'
+            @touchend.passive='emit("nodeClick",[$event,node])'
+            @mousedown.prevent='emit("dragStart",[$event,key])'
+            @touchstart.prevent='emit("dragStart",[$event,key])'
+            :x='node.x - getNodeSize(node, "width") / 2'
+            :y='node.y - getNodeSize(node, "height") / 2'
+            :style='nodeStyle(node)'
+            :title="node.name"
+            :class='nodeClass(node,["node-svg"])'
+            v-html='svgIcon(node).data'
+            v-bind='node._svgAttrs'
+            )
 
-    //- -> nodes
-    g.nodes#l-nodes(v-if='!noNodes')
-      template(v-for='(node,key) in nodes')
-        svg(v-if='svgIcon(node)'
+          //- default circle nodes
+          circle(v-else
           :key='key'
-          :viewBox='svgIcon(node).attrs.viewBox'
-          :width='getNodeSize(node, "width")'
-          :height='getNodeSize(node, "height")'
+          :r="getNodeSize(node) / 2"
           @click='emit("nodeClick",[$event,node])'
-          @touchend.passive='emit("nodeClick",[$event,node])'
-          @mousedown.prevent='emit("dragStart",[$event,key])'
-          @touchstart.prevent='emit("dragStart",[$event,key])'
-          :x='node.x - getNodeSize(node, "width") / 2'
-          :y='node.y - getNodeSize(node, "height") / 2'
+          @touchend.stop='emit("nodeClick",[$event,node])'
+          @mousedown.stop='emit("dragStart",[$event,key])'
+          @touchstart.stop='emit("dragStart",[$event,key])'
+          @mouseup.stop='emit("dragEnd",[$event])'
+          @touchend='emit("dragEnd",[$event])'
+          :cx="node.x"
+          :cy="node.y"
           :style='nodeStyle(node)'
           :title="node.name"
-          :class='nodeClass(node,["node-svg"])'
-          v-html='svgIcon(node).data'
+          :class="nodeClass(node)"
           v-bind='node._svgAttrs'
           )
 
-        //- default circle nodes
-        circle(v-else
-        :key='key'
-        :r="getNodeSize(node) / 2"
-        @click='emit("nodeClick",[$event,node])'
-        @touchend.passive='emit("nodeClick",[$event,node])'
-        @mousedown.prevent='emit("dragStart",[$event,key])'
-        @touchstart.prevent='emit("dragStart",[$event,key])'
-        :cx="node.x"
-        :cy="node.y"
-        :style='nodeStyle(node)'
-        :title="node.name"
-        :class="nodeClass(node)"
-        v-bind='node._svgAttrs'
-        )
+      //-> Links Labels
+      g.labels#link-labels(v-if='linkLabels')
+        text.link-label(v-for="link in links" :font-size="fontSize" )
+          textPath(v-bind:xlink:href="'#' + link.id" startOffset= "50%") {{ link.name }}
 
-    //-> Links Labels
-    g.labels#link-labels(v-if='linkLabels')
-      text.link-label(v-for="link in links" :font-size="fontSize" )
-        textPath(v-bind:xlink:href="'#' + link.id" startOffset= "50%") {{ link.name }}
-
-    //- -> Node Labels
-    g.labels#node-labels( v-if="nodeLabels")
-      text.node-label(v-for="node in nodes"
-        :x='node.x + (getNodeSize(node) / 2) + (fontSize / 2)'
-        :y='node.y + labelOffset.y'
-        :font-size="fontSize"
-        :class='(node._labelClass) ? node._labelClass : ""'
-        :stroke-width='fontSize / 8'
-      ) {{ node.name }}
+      //- -> Node Labels
+      g.labels#node-labels( v-if="nodeLabels")
+        text.node-label(v-for="node in nodes"
+          :x='node.x + labelOffset.x'
+          :y='node.y + labelOffset.y'
+          :font-size="fontSize"
+          :class='(node._labelClass) ? node._labelClass : ""'
+          :stroke-width='fontSize / 8'
+        ) {{ node.name }}
 </template>
 <script>
-import svgExport from '../lib/js/svgExport.js'
+import svgExport from "../lib/js/svgExport.js";
 
 export default {
-  name: 'svg-renderer',
+  name: "svg-renderer",
   props: [
-    'size',
-    'nodes',
-    'noNodes',
-    'selected',
-    'linksSelected',
-    'links',
-    'nodeSize',
-    'padding',
-    'fontSize',
-    'strLinks',
-    'linkWidth',
-    'nodeLabels',
-    'linkLabels',
-    'labelOffset',
-    'nodeSym'
+    "size",
+    "nodes",
+    "noNodes",
+    "selected",
+    "linksSelected",
+    "links",
+    "nodeSize",
+    "padding",
+    "fontSize",
+    "strLinks",
+    "linkWidth",
+    "nodeLabels",
+    "linkLabels",
+    "labelOffset",
+    "nodeSym",
   ],
 
   computed: {
-    nodeSvg () {
+    nodeSvg() {
       if (this.nodeSym) {
-        return svgExport.toObject(this.nodeSym)
+        return svgExport.toObject(this.nodeSym);
       }
-      return null
-    }
+      return null;
+    },
+  },
+  mounted() {
+    const svg = this.$refs.svg;
   },
   methods: {
-    getNodeSize (node, side) {
-      let size = node._size || this.nodeSize
-      if (side) size = node['_' + side] || size
-      return size
+    getNodeSize(node, side) {
+      let size = node._size || this.nodeSize;
+      if (side) size = node["_" + side] || size;
+      return size;
     },
-    svgIcon (node) {
-      return node.svgObj || this.nodeSvg
+    svgIcon(node) {
+      return node.svgObj || this.nodeSvg;
     },
-    emit (e, args) {
-      this.$emit('action', e, args)
+    emit(e, args) {
+      this.$emit("action", e, args);
     },
-    svgScreenShot (cb, toSvg, background, allCss) {
-      let svg = svgExport.export(this.$refs.svg, allCss)
+    svgScreenShot(cb, toSvg, background, allCss) {
+      let svg = svgExport.export(this.$refs.svg, allCss);
       if (!toSvg) {
-        if (!background) background = this.searchBackground()
-        let canvas = svgExport.makeCanvas(this.size.w, this.size.h, background)
+        if (!background) background = this.searchBackground();
+        let canvas = svgExport.makeCanvas(this.size.w, this.size.h, background);
         svgExport.svgToImg(svg, canvas, (err, img) => {
-          if (err) cb(err)
-          else cb(null, img)
-        })
+          if (err) cb(err);
+          else cb(null, img);
+        });
       } else {
-        cb(null, svgExport.save(svg))
+        cb(null, svgExport.save(svg));
       }
     },
-    linkClass (linkId) {
-      let cssClass = ['link']
+
+    linkClass(linkId) {
+      let cssClass = ["link"];
       if (this.linksSelected.hasOwnProperty(linkId)) {
-        cssClass.push('selected')
+        cssClass.push("selected");
       }
       if (!this.strLinks) {
-        cssClass.push('curve')
+        cssClass.push("curve");
       }
-      return cssClass
+      return cssClass;
     },
-    linkPath (link) {
+    linkPath(link) {
       let d = {
         M: [link.source.x | 0, link.source.y | 0],
-        X: [link.target.x | 0, link.target.y | 0]
-      }
+        X: [link.target.x | 0, link.target.y | 0],
+      };
       if (this.strLinks) {
-        return 'M ' + d.M.join(' ') + ' L' + d.X.join(' ')
+        return "M " + d.M.join(" ") + " L" + d.X.join(" ");
       } else {
-        d.Q = [link.source.x, link.target.y]
-        return 'M ' + d.M + ' Q ' + d.Q.join(' ') + ' ' + d.X
+        d.Q = [link.source.x, link.target.y];
+        return "M " + d.M + " Q " + d.Q.join(" ") + " " + d.X;
       }
     },
-    nodeStyle (node) {
-      return (node._color) ? 'fill: ' + node._color : ''
+    nodeStyle(node) {
+      return node._color ? "fill: " + node._color : "";
     },
-    linkStyle (link) {
-      let style = {}
-      if (link._color) style.stroke = link._color
-      return style
+    linkStyle(link) {
+      let style = {};
+      if (link._color) style.stroke = link._color;
+      return style;
     },
-    nodeClass (node, classes = []) {
-      let cssClass = (node._cssClass) ? node._cssClass : []
-      if (!Array.isArray(cssClass)) cssClass = [cssClass]
-      cssClass.push('node')
-      classes.forEach(c => cssClass.push(c))
-      if (this.selected[node.id]) cssClass.push('selected')
-      if (node.fx || node.fy) cssClass.push('pinned')
-      return cssClass
+    nodeClass(node, classes = []) {
+      let cssClass = node._cssClass ? node._cssClass : [];
+      if (!Array.isArray(cssClass)) cssClass = [cssClass];
+      cssClass.push("node");
+      classes.forEach((c) => cssClass.push(c));
+      if (this.selected[node.id]) cssClass.push("selected");
+      if (node.fx || node.fy) cssClass.push("pinned");
+      return cssClass;
     },
-    searchBackground () {
-      let vm = this
+    searchBackground() {
+      let vm = this;
       while (vm.$parent) {
-        let style = window.getComputedStyle(vm.$el)
-        let background = style.getPropertyValue('background-color')
-        let rgb = background.replace(/[^\d,]/g, '').split(',')
-        let sum = rgb.reduce((a, b) => parseInt(a) + parseInt(b), 0)
-        if (sum > 0) return background
-        vm = vm.$parent
+        let style = window.getComputedStyle(vm.$el);
+        let background = style.getPropertyValue("background-color");
+        let rgb = background.replace(/[^\d,]/g, "").split(",");
+        let sum = rgb.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+        if (sum > 0) return background;
+        vm = vm.$parent;
       }
-      return 'white'
+      return "white";
     },
-    spriteSymbol () {
-      let svg = this.nodeSym
+    spriteSymbol() {
+      let svg = this.nodeSym;
       if (svg) {
-        return svgExport.toSymbol(svg)
+        return svgExport.toSymbol(svg);
       }
     },
-    linkAttrs (link) {
-      let attrs = link._svgAttrs || {}
-      attrs['stroke-width'] = attrs['stroke-width'] || this.linkWidth
-      return attrs
-    }
-  }
-}
+    linkAttrs(link) {
+      let attrs = link._svgAttrs || {};
+      attrs["stroke-width"] = attrs["stroke-width"] || this.linkWidth;
+      return attrs;
+    },
+  },
+};
 </script>
--- src/example/utils.js
+++ src/example/utils.js
@@ -1,126 +1,467 @@
 // find links by node => [ links ] | null
 export const findLinks = (nodeId, links) => {
-  let nodeLinks = []
+  let nodeLinks = [];
   for (let link of links) {
-    if (link.sid === nodeId || link.tid === nodeId) nodeLinks.push(link)
+    if (link.sid === nodeId || link.tid === nodeId) nodeLinks.push(link);
   }
-  return (nodeLinks.length) ? nodeLinks : null
-}
+  return nodeLinks.length ? nodeLinks : null;
+};
 export const findNode = (nodes, nodeId) => {
-  let index = nodeExists(nodeId)
+  let index = nodeExists(nodeId);
   if (index) {
-    return nodes[index]
+    return nodes[index];
   }
-  return null
-}
+  return null;
+};
 // removes node by id => () => ( [newNodes] )
 export const removeNode = (nodeId, nodes, cb) => {
-  let index = nodes.findIndex(
-    (node) => { return node.id === nodeId }
-  )
+  let index = nodes.findIndex((node) => {
+    return node.id === nodeId;
+  });
   if (index > -1) {
-    nodes.splice(index, 1)
-    cb(nodes)
+    nodes.splice(index, 1);
+    cb(nodes);
   } else {
-    cb(null)
+    cb(null);
   }
-}
+};
 
 // removes orphaned links => { newLinks, removed }
 export const rebuildLinks = (nodes, links) => {
-  let newLinks = []
-  let removed = []
+  let newLinks = [];
+  let removed = [];
   for (let link of links) {
     if (nodeExists(link.sid, nodes) && nodeExists(link.tid, nodes)) {
-      newLinks.push(link)
+      newLinks.push(link);
     } else {
-      removed.push(link)
+      removed.push(link);
     }
   }
-  return { newLinks, removed }
-}
+  return { newLinks, removed };
+};
 
 // removes unlinked nodes => [ newNodes ]
 export const rebuildNodes = (links, nodes) => {
-  let newNodes = []
+  let newNodes = [];
   for (let node of nodes) {
     if (isLinked(node.id, links)) {
-      newNodes.push(node)
+      newNodes.push(node);
     }
   }
-  return newNodes
-}
+  return newNodes;
+};
 
 // finds node by id => boolean
 export const nodeExists = (nodeId, nodes) => {
-  let index = nodes.findIndex(
-    (node) => { return node.id === nodeId }
-  )
-  return (index > -1)
-}
+  let index = nodes.findIndex((node) => {
+    return node.id === nodeId;
+  });
+  return index > -1;
+};
 
 // Checks if node is linked => boolean
 const isLinked = (nodeId, links) => {
-  let index = links.findIndex(
-    (link) => { return (link.tid === nodeId || link.sid === nodeId) }
-  )
-  return (index > -1)
-}
+  let index = links.findIndex((link) => {
+    return link.tid === nodeId || link.sid === nodeId;
+  });
+  return index > -1;
+};
 
 // link formatter
 export const newLink = (id, sid, tid) => {
-  return { id, sid, tid }
-}
+  return { id, sid, tid };
+};
+const list = [
+  {
+    _id: "629df850a505ce65439b223d",
+    entity1: "小辣椒S1 Pro（移动4G）",
+    rel: "SIM卡类",
+    entity2: "双卡  ",
+  },
+  {
+    _id: "629df850a505ce65439b223e",
+    entity1: "小辣椒S1 Pro（移动4G）",
+    rel: "存储卡",
+    entity2: "MicroSD卡  ",
+  },
+  {
+    _id: "629df850a505ce65439b223f",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "价格",
+    entity2: "￥339[停产]",
+  },
+  {
+    _id: "629df850a505ce65439b2240",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "屏幕尺寸",
+    entity2: "5英寸  1280x720像素 ",
+  },
+  {
+    _id: "629df850a505ce65439b2241",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "CPU型号",
+    entity2: "展讯 SC9830  ",
+  },
+  {
+    _id: "629df850a505ce65439b2242",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "电池容量",
+    entity2: "2000mAh  ",
+  },
+  {
+    _id: "629df850a505ce65439b2243",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "前置摄像",
+    entity2: "200万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2244",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "后置摄像",
+    entity2: "500万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2245",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "RAM容量",
+    entity2: "1GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b2246",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "ROM容量",
+    entity2: "8GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b2247",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "SIM卡类",
+    entity2: "双卡  ",
+  },
+  {
+    _id: "629df850a505ce65439b2248",
+    entity1: "小辣椒红辣椒国民猴赛雷（移动4G）",
+    rel: "存储卡",
+    entity2: "MicroSD卡  ",
+  },
+  {
+    _id: "629df850a505ce65439b2249",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "价格",
+    entity2: "￥470[停产]",
+  },
+  {
+    _id: "629df850a505ce65439b224a",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "屏幕尺寸",
+    entity2: "5英寸  1280x720像素 ",
+  },
+  {
+    _id: "629df850a505ce65439b224b",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "电池容量",
+    entity2: "2300mAh  可拆卸式电池 ",
+  },
+  {
+    _id: "629df850a505ce65439b224c",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "前置摄像",
+    entity2: "500万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b224d",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "后置摄像",
+    entity2: "800万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b224e",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "RAM容量",
+    entity2: "2GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b224f",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "ROM容量",
+    entity2: "8GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b2250",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "SIM卡类",
+    entity2: "双卡（Micro SIM卡）  ",
+  },
+  {
+    _id: "629df850a505ce65439b2251",
+    entity1: "联想乐檬K3（K31-T3/移动4G）",
+    rel: "屏幕材质",
+    entity2: "TFT材质（IPS ",
+  },
+  {
+    _id: "629df850a505ce65439b2252",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "价格",
+    entity2: "￥799[停产]",
+  },
+  {
+    _id: "629df850a505ce65439b2253",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "屏幕尺寸",
+    entity2: "5.5英寸  1920x1080像素 ",
+  },
+  {
+    _id: "629df850a505ce65439b2254",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "CPU型号",
+    entity2: "联发科 Helio P10  ",
+  },
+  {
+    _id: "629df850a505ce65439b2255",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "电池容量",
+    entity2: "4100mAh  不可拆卸式电 ",
+  },
+  {
+    _id: "629df850a505ce65439b2256",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "前置摄像",
+    entity2: "500万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2257",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "后置摄像",
+    entity2: "1300万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2258",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "RAM容量",
+    entity2: "2GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b2259",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "ROM容量",
+    entity2: "16GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b225a",
+    entity1: "魅族魅蓝Note 3（全网通）",
+    rel: "SIM卡类",
+    entity2: "双卡（Nano S ",
+  },
+  {
+    _id: "629df850a505ce65439b225b",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "价格",
+    entity2: "￥550[停产]",
+  },
+  {
+    _id: "629df850a505ce65439b225c",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "屏幕尺寸",
+    entity2: "5英寸  1280x720像素 ",
+  },
+  {
+    _id: "629df850a505ce65439b225d",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "CPU型号",
+    entity2: "联发科 MT6753  ",
+  },
+  {
+    _id: "629df850a505ce65439b225e",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "电池容量",
+    entity2: "2500mAh  不可拆卸式电 ",
+  },
+  {
+    _id: "629df850a505ce65439b225f",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "前置摄像",
+    entity2: "500万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2260",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "后置摄像",
+    entity2: "1300万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2261",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "RAM容量",
+    entity2: "2GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b2262",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "ROM容量",
+    entity2: "16GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b2263",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "SIM卡类",
+    entity2: "双卡（Nano SIM卡）  ",
+  },
+  {
+    _id: "629df850a505ce65439b2264",
+    entity1: "360 手机f4（标准版/移动4G）",
+    rel: "存储卡",
+    entity2: "MicroSD卡  ",
+  },
+  {
+    _id: "629df850a505ce65439b2265",
+    entity1: "酷派8029（移动4G）",
+    rel: "价格",
+    entity2: "￥240[停产]",
+  },
+  {
+    _id: "629df850a505ce65439b2266",
+    entity1: "酷派8029（移动4G）",
+    rel: "屏幕尺寸",
+    entity2: "4英寸  800x480像素 ",
+  },
+  {
+    _id: "629df850a505ce65439b2267",
+    entity1: "酷派8029（移动4G）",
+    rel: "电池容量",
+    entity2: "1500mAh  可拆卸式电池 ",
+  },
+  {
+    _id: "629df850a505ce65439b2268",
+    entity1: "酷派8029（移动4G）",
+    rel: "后置摄像",
+    entity2: "200万像素  ",
+  },
+  {
+    _id: "629df850a505ce65439b2269",
+    entity1: "酷派8029（移动4G）",
+    rel: "RAM容量",
+    entity2: "512MB  ",
+  },
+  {
+    _id: "629df850a505ce65439b226a",
+    entity1: "酷派8029（移动4G）",
+    rel: "ROM容量",
+    entity2: "4GB  ",
+  },
+  {
+    _id: "629df850a505ce65439b226b",
+    entity1: "酷派8029（移动4G）",
+    rel: "存储卡",
+    entity2: "MicroSD卡  ",
+  },
+  {
+    _id: "629df850a505ce65439b226c",
+    entity1: "酷派8029（移动4G）",
+    rel: "传感器类",
+    entity2: "CMOS  ",
+  },
+  {
+    _id: "629df850a505ce65439b226d",
+    entity1: "酷派8029（移动4G）",
+    rel: "闪光灯",
+    entity2: "LED补光灯  ",
+  },
+  {
+    _id: "629df850a505ce65439b226e",
+    entity1: "酷派8029（移动4G）",
+    rel: "像素密度",
+    entity2: "233ppi  ",
+  },
+];
+import { nanoid } from "nanoid";
 
-// generates random links => [ links ]
-export const makeRandomLinks = (nodes, maxLinks) => {
-  let links = []
-  let id = 0
-  for (let node of nodes) {
-    let total = Math.floor(Math.random() * maxLinks)
-    for (let i = 0; i <= total; i++) {
-      let target = Math.floor(Math.random() * nodes.length)
-      let source = node.id
-      id++
-      links.push(newLink(id, source, target))
+export const getNode = () => {
+  const nodes = [];
+  for (let i = 0; i < list.length; i++) {
+    const { entity1, entity2 } = list[i];
+    if (!nodes.filter((item) => item.name && item.name === entity1).length) {
+      nodes.push({
+        name: entity1,
+        id: nanoid(),
+      });
+    }
+    if (!nodes.filter((item) => item.name && item.name === entity2).length) {
+      nodes.push({
+        name: entity2,
+        id: nanoid(),
+      });
     }
   }
-  return links
-}
+  return nodes;
+};
+export const getLink = (allNodes) => {
+  const links = [];
+
+  for (let i = 0; i < list.length; i++) {
+    const { entity1, entity2, rel } = list[i];
+    const sid = allNodes.find((item) => item.name === entity1).id;
+    const tid = allNodes.find((item) => item.name === entity2).id;
+
+    links.push({
+      sid,
+      tid,
+      name: rel,
+    });
+  }
+  return links;
+};
+// generates random links => [ links ]
+export const makeRandomLinks = (nodes, maxLinks) => {
+  let links = [];
+  let id = 0;
+  for (let node of nodes) {
+    let total = Math.floor(Math.random() * maxLinks);
+    for (let i = 0; i <= total; i++) {
+      let target = Math.floor(Math.random() * nodes.length);
+      let source = node.id;
+      id++;
+      links.push(newLink(id, source, target));
+    }
+  }
+
+  return links;
+};
 
 // random node name
 const newNodeName = () => {
-  return Math.random().toString(36).substring(7)
-}
+  return Math.random().toString(36).substring(7);
+};
 
 // node formatter
 export const newNode = (nodeId) => {
-  return { id: nodeId, name: newNodeName() }
-}
+  return { id: nodeId, name: newNodeName() };
+};
 
 // generates random nodes => [ nodes ]
 export const makeRandomNodes = (maxNodes) => {
-  let nodes = Array.apply(null, { length: maxNodes })
-    .map((value, index) => { return newNode(index) })
-  return nodes
-}
+  let nodes = Array.apply(null, { length: maxNodes }).map((value, index) => {
+    return newNode(index);
+  });
+  // console.log(nodes);
+  return nodes;
+};
 
 // vue custom event handler
 export const methodCall = (vm, action, args) => {
-  let method = vm[action]
-  if (typeof method === 'function') {
-    if (args) method(...args)
-    else method()
+  let method = vm[action];
+  if (typeof method === "function") {
+    if (args) method(...args);
+    else method();
   } else {
-    console.error('Call to undefined method:', action)
+    console.error("Call to undefined method:", action);
   }
-}
+};
 
 // vue event wrapper
 export const emitEvent = (vm, action, args) => {
   if (vm.$data.conf && vm.$data.conf.allEventsAs) {
-    let evName = vm.$data.conf.allEventsAs
-    return vm.$emit(evName, action, args)
+    let evName = vm.$data.conf.allEventsAs;
+    return vm.$emit(evName, action, args);
   }
-  return vm.$emit(action, ...args)
-}
+  return vm.$emit(action, ...args);
+};
